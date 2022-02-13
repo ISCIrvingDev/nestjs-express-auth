1ca624c88aebba6fec45f2c8230a4b7d6d40fe08
ISCIrvingDev <ISCIrvingDev@gmail.com>
2022-02-13 01:41:49 -0600

    Create the login route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './application/auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('auth/login')
+  // Aunque funciona de esta manera, NestJS recomienda crear una clase que se encargue de esto. En este caso seria la clase -> "LocalAuthGuard"
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
+  async login(@Request() req) {
+    return req.user;
   }
 }
